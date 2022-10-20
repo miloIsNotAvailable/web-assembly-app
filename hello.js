26ddb456a88a9b8c3b0ce0496c9bdfec0fe48c01
unknown <milosz20031207@gmail.com>
2022-10-20 18:55:15 +0200

    initial commit

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 
 
 // The Module object: Our interface to the outside world. We import
+
 // and export values on it. There are various ways Module can be used:
 // 1. Not defined. We create it here
 // 2. A function parameter, function(Module) { ..generated code.. }
