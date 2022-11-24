6cdd16a31661fb55400a573625d8b3a702d8bb99
unknown <milosz20031207@gmail.com>
2022-11-24 19:59:51 +0100

    feature: made includes work

--- web_assembly_cpp/hello.cpp
+++ web_assembly_cpp/hello.cpp
@@ -6,6 +6,7 @@
 #include <EGL/egl.h>
 #include<math.h>
 #include <vector>
+#include "./app.cpp"
 
 // #ifndef __gles2_gl3_h_
 // #define __gles2_gl3_h_ 1
@@ -168,77 +169,6 @@ EM_BOOL wheel_callback(int eventType, const EmscriptenWheelEvent *e, void *userD
     return 1;
 }
 
-class Render {
-    private: 
-        const char *fragmentShaderSource = "\n"
-        "#ifdef GL_ES\n"
-        "precision highp float;\n"
-        "#endif\n"
-        "\n"    
-        "uniform vec2 u_resolution;\n"
-        "uniform vec2 u_mouse;\n"
-        "uniform float u_time;\n"
-        "uniform vec3 color;\n"
-        "void main(){\n"
-        "vec2 st = gl_FragCoord.xy/u_resolution.xy;\n"
-        "st.x *= u_resolution.x/u_resolution.y;\n"
-        "vec2 coord = gl_PointCoord - vec2(0.5);  //from [0,1] to [-0.5,0.5]\n"
-        "if(length(coord) > .5 )                  //outside of circle radius?\n"
-        "    discard;\n"
-        "\n"    
-        "gl_FragColor = vec4(color, 1.0);\n"
-        "}\0";
-        
-        const char *vertexShaderSource = "\n"
-        "#ifdef GL_ES\n"
-        "precision highp float;\n"
-        "#endif\n"
-        "attribute vec3 aPos;\n"        
-        "uniform mat4 SM;\n"    
-        "uniform float SMX;\n"    
-        "uniform float TMX;\n"    
-        "uniform mat4 TM;\n"    
-        "\n" 
-        "void main() {\n"
-        "   float u = aPos.x;\n"
-        "   float v = aPos.y;\n"
-        "    gl_Position =  vec4(aPos.x * SMX, aPos.y * SMX + TMX, aPos.z, 1.);\n"
-        "    gl_PointSize = 10.;\n"
-        "}\n\0";
-
-        int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-        int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    public:
-        int shaderProgram = glCreateProgram();
-
-        void initShader( Render r ) {
-            // vertex shader
-            glShaderSource(r.vertexShader, 1, &r.vertexShaderSource, NULL);
-            glCompileShader(r.vertexShader);
-            // check for shader compile errors
-
-            // fragment shader
-            glShaderSource(r.fragmentShader, 1, &r.fragmentShaderSource, NULL);
-            glCompileShader(r.fragmentShader);
-
-            // link shaders
-            glAttachShader(r.shaderProgram, r.vertexShader);
-            glAttachShader(r.shaderProgram, r.fragmentShader);
-            
-            glLinkProgram(r.shaderProgram);
-            clearShader( r );
-            glUseProgram( r.shaderProgram );
-            
-        }
-
-        void clearShader( Render r ) {
-            
-            // glDetachShader( r.shaderProgram, r.fragmentShader );
-            // glDetachShader( r.shaderProgram, r.vertexShader );
-            glDeleteShader( r.vertexShader );
-            glDeleteShader( r.fragmentShader );
-        }
-};
 
 class Vertex {
     public: 
